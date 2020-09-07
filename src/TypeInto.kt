0782aecddcebf1be6d613cf55f91b493183a515e
elham ahmed alnini <elham307409001@gmail.com>
2020-09-07 19:58:28 +0300

    Initial commit

---
+++ src/TypeInto.kt
@@ -0,0 +1,26 @@
+
+fun main(args: Array<String>) {
+
+    var experiencePoints:Int =5
+    val playerName:String ="Estragon"
+
+    experiencePoints+=5
+
+    println("It is "+experiencePoints)
+    println("It is "+playerName)
+
+    var hasSteed=false
+
+    if(hasSteed==true){
+        println("have one yet")
+    }else{
+        println("does not have one yet")
+    }
+
+    var reversePlayerName=playerName.reversed()
+
+    println("the reverse Player Name is "+reversePlayerName)
+}
+
+class TypeInto {
+}
